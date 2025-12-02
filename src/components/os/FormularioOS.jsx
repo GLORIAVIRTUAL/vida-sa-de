53375e573d369570010b4267945f7974967da92f
Glória Virtual <dmpetrolina@gmail.com>
2025-12-02 15:56:08 +0000

    File changes

--- src/components/os/FormularioOS.jsx
+++ src/components/os/FormularioOS.jsx
@@ -13,6 +13,7 @@ import { AlertCircle } from "lucide-react";
 import { OrdemServico } from "@/entities/all";
 import { format } from 'date-fns';
 import { useToast } from "@/components/ui/use-toast"; // Import useToast
+import { base44 } from "@/api/base44Client";
 
 const formasPagamento = ["Dinheiro", "Cartão Débito", "Cartão Crédito", "PIX", "Transferência", "Convênio", "Múltiplas Formas"];
 
