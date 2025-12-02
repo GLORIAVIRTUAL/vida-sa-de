689770158503222fbc0f4346f129e3e74fd0d22e
Glória Virtual <dmpetrolina@gmail.com>
2025-12-02 19:55:26 +0000

    File changes

--- functions/createOrdemServico.ts
+++ functions/createOrdemServico.ts
@@ -75,7 +75,7 @@ Deno.serve(async (req) => {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${API_TOKEN}`
+                        'bearer': API_TOKEN
                     },
                     body: JSON.stringify(payloadEvolu)
                 });
--- functions/createVendaCartao.ts
+++ functions/createVendaCartao.ts
@@ -166,7 +166,7 @@ Deno.serve(async (req) => {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${API_TOKEN}`
+                        'bearer': API_TOKEN
                     },
                     body: JSON.stringify(payloadEvolu)
                 });
