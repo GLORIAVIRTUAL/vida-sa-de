9d4c22cab9489cfe97c11184469bf653a34c4f53
Glória Virtual <dmpetrolina@gmail.com>
2026-01-15 21:48:22 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -34,7 +34,6 @@ import VendaCartao from './pages/VendaCartao';
 import VerificacaoAssinatura from './pages/VerificacaoAssinatura';
 import WebhookLogs from './pages/WebhookLogs';
 import ordemServico from './pages/ordem-servico';
-import ConfirmarPresenca from './pages/ConfirmarPresenca';
 import __Layout from './Layout.jsx';
 
 
@@ -75,7 +74,6 @@ export const PAGES = {
     "VerificacaoAssinatura": VerificacaoAssinatura,
     "WebhookLogs": WebhookLogs,
     "ordem-servico": ordemServico,
-    "ConfirmarPresenca": ConfirmarPresenca,
 }
 
 export const pagesConfig = {
