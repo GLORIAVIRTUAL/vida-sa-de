f3106cfb89e16140b3e42ea066adfbb68918c1f2
Glória Virtual <dmpetrolina@gmail.com>
2026-01-16 12:09:48 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -358,7 +358,7 @@ export default function Layout({ children, currentPageName }) {
 
   useEffect(() => {
     // Lista de páginas que devem ser públicas (sem necessidade de login)
-          const publicPages = ['AgendamentoOnline', 'Confirmar'];
+          const publicPages = ['AgendamentoOnline'];
     
     if (publicPages.includes(currentPageName)) {
         setIsPublicPage(true);
