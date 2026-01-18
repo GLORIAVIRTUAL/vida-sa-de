24b243db95978bd5e1eba1545e43231416b86f1d
Glória Virtual <dmpetrolina@gmail.com>
2026-01-18 12:58:02 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -199,7 +199,7 @@ function MainLayout({ children, currentPageName, currentUser, onUserUpdate }) {
                     className="flex items-center px-4 py-2 text-sm font-medium rounded-lg transition-colors text-gray-700 hover:bg-gray-100"
                     onClick={() => setSidebarOpen(false)}
                     >
-                    <item.icon className="w-5 h-5 mr-3 flex-shrink-0" />
+                    {React.createElement(item.icon, { className: "w-5 h-5 mr-3 flex-shrink-0" })}
                     <span>{item.name}</span>
                     </a>
                 );
