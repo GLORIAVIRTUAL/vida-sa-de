2a670c83b273532b480853f04e13da16e719bb5c
Glória Virtual <dmpetrolina@gmail.com>
2026-02-07 18:26:53 +0000

    File changes

--- src/components/agendamentos/FormularioAgendamento.jsx
+++ src/components/agendamentos/FormularioAgendamento.jsx
@@ -933,7 +933,7 @@ export default function FormularioAgendamento({ agendamento, dadosIniciais, todo
     };
 
     loadHorarios();
-  }, [formData.medico_id, formData.data_agendamento, formData.tipo_servico, formData.is_encaixe, carregarHorarios, agendamento]);
+  }, [formData.medico_id, formData.data_agendamento, formData.tipo_servico, formData.is_encaixe, carregarHorarios, agendamento, todosAgendamentos]);
 
   // NOVO: Carregar horários do médico selecionado para consulta em múltiplos serviços
   const carregarHorariosParaMedicoMultiplo = useCallback(async (medicoId) => {
