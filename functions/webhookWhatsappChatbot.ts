abbbd0e70e1f9f290405d02f8222063ee2637072
Glória Virtual <dmpetrolina@gmail.com>
2026-02-10 11:17:19 +0000

    File changes

--- functions/webhookWhatsappChatbot.ts
+++ functions/webhookWhatsappChatbot.ts
@@ -214,9 +214,10 @@ Deno.serve(async (req) => {
       if (contato) {
         // Reativar conversa se estava finalizada
         if (contato.conversa_finalizada) {
-          console.log('🔄 Reativando conversa finalizada - mantendo histórico anterior');
+          console.log('🔄 Reativando conversa finalizada - limpando histórico para conversa nova');
           await base44.asServiceRole.entities.Contato.update(contato.id, {
             conversa_finalizada: false,
+            historico_mensagens: [],
             mensagens_pendentes: [],
             ultimo_timestamp_pendente: null,
             atendimento_humano: false,
