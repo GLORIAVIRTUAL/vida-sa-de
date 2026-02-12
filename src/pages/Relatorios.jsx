e647f3228e8638669daaf1b58dc14da5426005b4
Glória Virtual <dmpetrolina@gmail.com>
2026-02-12 13:29:53 +0000

    File changes

--- src/pages/Relatorios.jsx
+++ src/pages/Relatorios.jsx
@@ -1118,15 +1118,26 @@ export default function Relatorios() {
                       porCategoriaLocal[nomeCat].valor += (os.valor_final || 0);
                     });
                     
-                    // Agrupar por forma de pagamento
+                    // Agrupar por forma de pagamento (desmembrando "Múltiplas Formas")
                     const porPagamentoLocal = {};
                     osMedico.forEach(os => {
-                      const forma = os.forma_pagamento || 'Não informado';
-                      if (!porPagamentoLocal[forma]) {
-                        porPagamentoLocal[forma] = { quantidade: 0, valor: 0 };
+                      if (os.forma_pagamento === 'Múltiplas Formas' && os.pagamentos_detalhados && os.pagamentos_detalhados.length > 0) {
+                        os.pagamentos_detalhados.forEach(pg => {
+                          const forma = pg.forma || 'Não informado';
+                          if (!porPagamentoLocal[forma]) {
+                            porPagamentoLocal[forma] = { quantidade: 0, valor: 0 };
+                          }
+                          porPagamentoLocal[forma].quantidade++;
+                          porPagamentoLocal[forma].valor += (pg.valor || 0);
+                        });
+                      } else {
+                        const forma = os.forma_pagamento || 'Não informado';
+                        if (!porPagamentoLocal[forma]) {
+                          porPagamentoLocal[forma] = { quantidade: 0, valor: 0 };
+                        }
+                        porPagamentoLocal[forma].quantidade++;
+                        porPagamentoLocal[forma].valor += (os.valor_final || 0);
                       }
-                      porPagamentoLocal[forma].quantidade++;
-                      porPagamentoLocal[forma].valor += (os.valor_final || 0);
                     });
                     
                     return (
