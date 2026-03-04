432b95af35d41c4105546b869cc26b142fffb58b
Glória Virtual <dmpetrolina@gmail.com>
2026-03-04 10:45:23 +0000

    File changes

--- functions/zapiWebhook.ts
+++ functions/zapiWebhook.ts
@@ -345,7 +345,7 @@ async function processarMensagemRecebida(base44, payload) {
                         historico_mensagens: historicoComSeparador.slice(-200),
                         mensagens_pendentes: [],
                         ultimo_timestamp_pendente: null,
-                        atendimento_humano: false,
+                        atendimento_humano: true, // Alterado para true conforme solicitado
                         atendente_atual: null,
                         atendente_id: null
                     });
