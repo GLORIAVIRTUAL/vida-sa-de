0caa5c61e32f4464c1e6b31a4e7ed0a590bfdefc
Glória Virtual <dmpetrolina@gmail.com>
2026-03-14 09:01:40 +0000

    File changes

--- functions/getAvailableSlots.ts
+++ functions/getAvailableSlots.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.7.0';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
 
 const diasSemanaMap = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
