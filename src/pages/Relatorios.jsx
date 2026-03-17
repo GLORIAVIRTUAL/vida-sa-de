b9d600c48bf667e7a326c6d3e737a22e9d02dd3f
Glória Virtual <dmpetrolina@gmail.com>
2026-03-17 13:23:14 +0000

    File changes

--- src/pages/Relatorios.jsx
+++ src/pages/Relatorios.jsx
@@ -1306,6 +1306,7 @@ export default function Relatorios() {
                         <TableHead className="text-center">Atendimentos</TableHead>
                         <TableHead className="text-right">Faturado</TableHead>
                         <TableHead className="text-right">Repasse</TableHead>
+                        <TableHead className="text-right">Imposto</TableHead>
                         <TableHead className="text-right">Clínica</TableHead>
                         <TableHead className="text-right">% do Total</TableHead>
                       </TableRow>
@@ -1313,7 +1314,10 @@ export default function Relatorios() {
                     <TableBody>
                       {Object.entries(estatisticas.porMedicoId)
                         .sort((a, b) => b[1].valor - a[1].valor)
-                        .map(([id, dados]) => (
+                        .map(([id, dados]) => {
+                          const osMedico = dadosFiltrados.filter(os => obterMedicoIdReal(os) === id);
+                          const impostoMedico = osMedico.reduce((acc, os) => acc + (os.valor_imposto || 0), 0);
+                          return (
                           <TableRow key={id}>
                             <TableCell className="font-medium">{dados.nome}</TableCell>
                             <TableCell>
@@ -1322,7 +1326,8 @@ export default function Relatorios() {
                             <TableCell className="text-center">{dados.quantidade}</TableCell>
                             <TableCell className="text-right">{formatCurrency(dados.valor)}</TableCell>
                             <TableCell className="text-right text-purple-600">{formatCurrency(dados.repasse)}</TableCell>
-                            <TableCell className="text-right text-green-600">{formatCurrency(dados.valor - dados.repasse)}</TableCell>
+                            <TableCell className="text-right text-amber-600">{formatCurrency(impostoMedico)}</TableCell>
+                            <TableCell className="text-right text-green-600">{formatCurrency(dados.valor - dados.repasse - impostoMedico)}</TableCell>
                             <TableCell className="text-right">
                               {estatisticas.totalVendido > 0 
                                 ? ((dados.valor / estatisticas.totalVendido) * 100).toFixed(1) + '%'
