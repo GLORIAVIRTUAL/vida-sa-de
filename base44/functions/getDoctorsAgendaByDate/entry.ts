16c64107d324c9476a56cccf3cae2a2d37a504ed
Glória Virtual <dmpetrolina@gmail.com>
2026-03-25 18:04:02 +0000

    File changes

--- base44/functions/getDoctorsAgendaByDate/entry.ts
+++ base44/functions/getDoctorsAgendaByDate/entry.ts
@@ -213,9 +213,9 @@ const shouldUseBookedDoctorsOnly = (queryText = '') => {
   const timeWords = 'amanha|amanhã|hoje|segunda|terca|quarta|quinta|sexta|sabado|domingo';
   return new RegExp(`(quem|quais).*(vai|vao|vão|est[aã]o|estar|tem|t[eê]m).*(atender|atendendo|na clinica|ai na clinica|agenda|hor[aá]rio)`).test(normalizedQuery)
     || new RegExp(`(medicos|m[eé]dicos|profissionais).*(vai|vao|vão|est[aã]o|estar|tem|t[eê]m).*(${timeWords}|agenda|hor[aá]rio)`).test(normalizedQuery)
-    || new RegExp(`(vai|vao|vão|est[aã]o|estar).*(ter\\s+)?(medicos|m[eé]dicos|profissionais).*(atender|atendendo|${timeWords})`).test(normalizedQuery)
-    || new RegExp(`(tem|vai ter).*(medico|m[eé]dico|profissional).*(atendendo|${timeWords}|agenda|hor[aá]rio)`).test(normalizedQuery)
-    || new RegExp(`(quais|qual).*(medico|m[eé]dico|profissional).*(atende|atendem|atendendo|${timeWords})`).test(normalizedQuery);
+    || new RegExp(`(vai|vao|vão|est[aã]o|estar).*(ter\\s+)?(medicos|m[eé]dicos|profissionais).*(atender|atendendo).*(${timeWords})`).test(normalizedQuery)
+    || new RegExp(`(tem|vai ter).*(medico|m[eé]dico|profissional).*(atendendo|agenda|hor[aá]rio).*(${timeWords})`).test(normalizedQuery)
+    || new RegExp(`(quais|qual).*(medico|m[eé]dico|profissional).*(atende|atendem|atendendo).*(${timeWords})`).test(normalizedQuery);
 };
 
 Deno.serve(async (req) => {
