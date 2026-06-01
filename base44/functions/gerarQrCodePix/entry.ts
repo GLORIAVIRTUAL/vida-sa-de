4b55cd0af76cc506d41de5fffb2663bafdee6b99
Glória Virtual <dmpetrolina@gmail.com>
2026-06-01 13:37:49 +0000

    File changes

--- base44/functions/gerarQrCodePix/entry.ts
+++ base44/functions/gerarQrCodePix/entry.ts
@@ -104,6 +104,8 @@ Deno.serve(async (req) => {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': `Basic ${basicAuth}`,
         'Content-Length': Buffer.byteLength(tokenBody),
+        'User-Agent': 'Mozilla/5.0 (compatible; GloriaClinica/1.0)',
+        'Accept': 'application/json',
       },
       body: tokenBody,
       cert: certPem,
